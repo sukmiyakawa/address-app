41973a2adaf483996ac13f0a6a601a8a323ae626
Suguru Miyakawa <suk.web96@gmail.com>
2025-10-03 09:46:10 +0900

    マニュアル追加

--- src/components/Manual.tsx
+++ src/components/Manual.tsx
@@ -3,7 +3,7 @@ const Manual = () => {
     <>
       <div className="manual">
         <h2 className="manualTitle">Manual</h2>
-        <img src="./src/assets/img/manual.png" />
+        <img src="assets/img/manual.png" />
       </div>
     </>
   );
